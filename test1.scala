357f1c7c9f36a865a1e26eef1a73cc2a067df45d
Satoshi Matsuok <matsuoka@ni.aist.go.jp>
2019-09-18 13:35:14 +0900

    "Hello World!" three times

--- test1.scala
+++ test1.scala
@@ -2,6 +2,7 @@ object Main {
     def main(args: Array[String]): Unit = {
         println("Hello World!")
         println("Hello World Again!")
+        println("Hello World Again Again!")
     }
 
 }

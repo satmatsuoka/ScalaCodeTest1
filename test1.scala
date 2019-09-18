8adff7d46ce2fbfa7184913ff34bf073ef68053a
Satoshi Matsuok <matsuoka@ni.aist.go.jp>
2019-09-18 13:43:59 +0900

    random "Hello World!"

--- test1.scala
+++ test1.scala
@@ -1,8 +1,13 @@
+import scala.util.Random
+
 object Main {
     def main(args: Array[String]): Unit = {
-        println("Hello World!")
-        println("Hello World Again!")
-        println("Hello World Again Again!")
+        var r = new Random
+        val n = r.nextInt(10)
+        
+        for (i <- 1 to n) {
+          println("Hello World!")
+        }
     }
 
 }
